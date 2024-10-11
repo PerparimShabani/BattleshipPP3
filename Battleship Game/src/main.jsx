311df7d2150f562a5d6e5894c6137a645c66896d
Perparim <perparimshabani.97@gmail.com>
2024-10-11 21:01:44 +0200

    spell check was done to update

--- Battleship Game/src/main.jsx
+++ Battleship Game/src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App.jsx";
-import "./Index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
+import App from "./App.jsx"
+import "./Index.css"
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
