e9291c660a83f3b9f806c2fb403f7d43c2a3d742
Perparim <perparimshabani.97@gmail.com>
2024-10-11 20:59:21 +0200

    updated the main.jsx file

--- Battleship Game/src/main.jsx
+++ Battleship Game/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx';
 import './Index.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+        <Router basename="/BattleshipPP3">
+            <App />
+        </Router>
+    </React.StrictMode>,
+)
